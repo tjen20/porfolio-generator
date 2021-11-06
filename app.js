33fc1d1f0686398ed02f03ee34b35501abcfcfc3
Teri Jensen <89708940+tjen20@users.noreply.github.com>
2021-11-06 12:08:35 -0600

    adding requirements

--- app.js
+++ app.js
@@ -20,7 +20,15 @@ const promptUser = () => {
       {
         type: 'input',
         name: 'github',
-        message: 'Enter your GitHub Username'
+        message: 'Enter your GitHub Username (Required)',
+        validate: nameInput => {
+            if (nameInput) {
+              return true;
+            } else {
+              console.log('Please enter your name!');
+              return false;
+            }
+          }
       },
       {
         type: 'input',
@@ -44,16 +52,32 @@ if (!portfolioData.projects) {
     portfolioData.projects = [];
   }
     return inquirer.prompt([
-      {
-        type: 'input',
-        name: 'name',
-        message: 'What is the name of your project?'
-      },
+        {
+            type: 'input',
+            name: 'name',
+            message: 'What is your name? (Required)',
+            validate: nameInput => {
+              if (nameInput) {
+                return true;
+              } else {
+                console.log('Please enter your name!');
+                return false;
+              }
+            }
+          },
       {
         type: 'input',
         name: 'description',
-        message: 'Provide a description of the project (Required)'
-      },
+        message: 'Provide a description of the project (Required)',
+        validate: nameInput => {
+            if (nameInput) {
+              return true;
+            } else {
+              console.log('Please enter your name!');
+              return false;
+            }
+          }
+        },
       {
         type: 'checkbox',
         name: 'languages',
@@ -63,7 +87,15 @@ if (!portfolioData.projects) {
       {
         type: 'input',
         name: 'link',
-        message: 'Enter the GitHub link to your project. (Required)'
+        message: 'Enter the GitHub link to your project. (Required)',
+        validate: nameInput => {
+            if (nameInput) {
+              return true;
+            } else {
+              console.log('Please enter your name!');
+              return false;
+            }
+          }
       },
       {
         type: 'confirm',
